a6cf576776bfea7ee1a48c087ff99ba1d612c2b7
Firzus <lprieu@gaming.tech>
2022-04-06 19:37:57 +0200

    affichage + collision

--- time-out/js/script.js
+++ time-out/js/script.js
@@ -2,6 +2,7 @@ var config = {
     type: Phaser.AUTO,
     width: 1920,
     height: 1080,
+    scale: { zoom: 2 },
     physics: {
         default: 'arcade',
         arcade: {
@@ -22,26 +23,33 @@ function preload ()
 {
     this.load.image('background', 'assets/bg.png');
 
-    this.load.image('asset_map', 'assets/asset_map.png')
+    this.load.image('tiles', 'assets/asset_map.png')
 
-    this.load.tilemapTiledJSON('tilemap', 'projet_map.json')
+    this.load.tilemapTiledJSON('test', 'assets/projet_map.json')
 }
-
-var bg;
 
 function create ()
 {
     this.add.image(0, 0, 'background').setOrigin(0, 0);
  
-    const map = this.add.tilemap({key:'tilemap'});
+    const map = this.make.tilemap({key:'test'});
  
-    const tileset = map.addTilesetImage('asset_map', 'asset_map');
+    const tileset = map.addTilesetImage('asset_map', 'tiles');
  
     map.createLayer('1', tileset);
 
     map.createLayer('2', tileset);
 
-    map.createLayer('3', tileset);
+    const collision = map.createLayer('3', tileset);
+
+    collision.setCollisionByProperty({ collides : true})
+    
+    const debugGraphics = this.add.graphics().setAlpha(0.7)
+    collision.renderDebug(debugGraphics, {
+        tileColor: null,
+        collidingTileColor: new Phaser.Display.Color(243, 234, 48, 255),
+        faceColor: new Phaser.Display.Color(40, 39, 37, 255)
+    })
 }
 
 function update ()
