3810571655bb710338826158438593ff105482fa
unknown <lprieu@gamingcampus.local>
2022-04-06 13:08:49 +0200

    add content

--- time-out/js/script.js
+++ time-out/js/script.js
@@ -21,16 +21,35 @@ var game = new Phaser.Game(config);
 function preload ()
 {
     this.load.image('background', 'assets/bg.png');
-    this.load.tilemap('level1', 'assets/map.json', null, Phaser.Tilemap.TILED_JSON);
+    this.load.image('base_tiles', 'assets/map.png');
+    this.load.tilemapTiledJSON('tilemap', 'assets/map.json');
 }
 
 var bg;
-var map;
+var tileset;
+var layer;
 
 function create ()
 {
     this.add.image(0, 0, 'background').setOrigin(0, 0);
-    bg.fixedToCamera = true;
+    //bg.fixedToCamera = true;
+
+    this.add.image(0, 0, 'base_tiles');
+    //map.setCollisionByExclusion([ 13, 14, 15, 16, 46, 47, 48, 49, 50, 51 ]); SET COLLISION
+
+    // create the Tilemap
+    const map = this.make.tilemap({ key: 'tilemap' })
+
+    // add the tileset image we are using
+	const tileset = map.addTilesetImage('standard_tiles', 'base_tiles')
+
+    // create the layers we want in the right order
+	map.createStaticLayer('Background', tileset)
+
+    // "Ground" layer will be on top of "Background" layer
+	map.createStaticLayer('Ground', tileset)
+
+    // the remaining tile layers ...
 }
 
 function update ()
