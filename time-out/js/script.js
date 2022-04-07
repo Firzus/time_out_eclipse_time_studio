deeeefeeac5f379c5ae5b6586c4452339ab9e77b
Firzus <lprieu@gaming.tech>
2022-04-07 06:04:03 +0200

    add content

--- time-out/js/script.js
+++ time-out/js/script.js
@@ -53,49 +53,119 @@ function create() {
 
     const spawnPoint = map.findObject("Objects", obj => obj.name === "Spawn Point");
 
-    /*// Régler la taille du personnage //
-
     player = this.physics.add
     .sprite(spawnPoint.x, spawnPoint.y, "atlas", "misa-front")
-    .setSize(30, 40)
-    .setOffset(0, 24);
+    //.setSize(30, 40)  Régler la taille du personnage
+    //.setOffset(0, 24);  Régler la taille du personnage
 
-    */// Régler la taille du personnage //
-
-    player = this.physics.add.sprite(spawnPoint.x, spawnPoint.y, "player_template", "player.png");
     this.physics.add.collider(player, worldLayer);
 
-    const debugGraphics = this.add.graphics().setAlpha(0.75);
-    worldLayer.renderDebug(debugGraphics, {
-        tileColor: null, // Color of non-colliding tiles
-        collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
-        faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
+    /* Animation du personnage
+
+    const anims = this.anims;
+    anims.create({
+        key: "misa-left-walk",
+        frames: anims.generateFrameNames("atlas", { prefix: "misa-left-walk.", start: 0, end: 3, zeroPad: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+    anims.create({
+        key: "misa-right-walk",
+        frames: anims.generateFrameNames("atlas", { prefix: "misa-right-walk.", start: 0, end: 3, zeroPad: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+    anims.create({
+        key: "misa-front-walk",
+        frames: anims.generateFrameNames("atlas", { prefix: "misa-front-walk.", start: 0, end: 3, zeroPad: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+    anims.create({
+        key: "misa-back-walk",
+        frames: anims.generateFrameNames("atlas", { prefix: "misa-back-walk.", start: 0, end: 3, zeroPad: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    */
+
+    const camera = this.cameras.main;
+    camera.startFollow(player);
+    camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+    // Help text that has a "fixed" position on the screen
+    this.add
+        .text(16, 16, 'Pour jouer dans de meilleures conditions,\npressez la touche F11', {
+            font: "18px monospace",
+            fill: "#000000",
+            padding: { x: 20, y: 10 },
+            backgroundColor: "#ffffff"
+        })
+        .setScrollFactor(0)
+        .setDepth(30);
+
+    // Debug graphics
+    this.input.keyboard.once("keydown-D", event => {
+        // Turn on physics debugging to show player's hitbox
+        this.physics.world.createDebugGraphic();
+
+        // Create worldLayer collision graphic above the player, but below the help text
+        const graphics = this.add
+            .graphics()
+            .setAlpha(0.75)
+            .setDepth(20);
+        worldLayer.renderDebug(graphics, {
+            tileColor: null, // Color of non-colliding tiles
+            collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
+            faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
+        });
     });
 }
 
 function update(time, delta) {
 
+    const speed = 175;
+    const prevVelocity = player.body.velocity.clone();
+
     // Stop any previous movement from the last frame
     player.body.setVelocity(0);
 
-    /*
-
     // Horizontal movement
     if (cursors.left.isDown) {
-        player.body.setVelocityX(-100);
+        player.body.setVelocityX(-speed);
     } else if (cursors.right.isDown) {
-        player.body.setVelocityX(100);
+        player.body.setVelocityX(speed);
     }
 
     // Vertical movement
     if (cursors.up.isDown) {
-        player.body.setVelocityY(-100);
+        player.body.setVelocityY(-speed);
     } else if (cursors.down.isDown) {
-        player.body.setVelocityY(100);
+        player.body.setVelocityY(speed);
     }
 
     // Normalize and scale the velocity so that player can't move faster along a diagonal
     player.body.velocity.normalize().scale(speed);
 
-    */
+    // Update the animation last and give left/right animations precedence over up/down animations
+    if (cursors.left.isDown) {
+        player.anims.play("misa-left-walk", true);
+    } else if (cursors.right.isDown) {
+        player.anims.play("misa-right-walk", true);
+    } else if (cursors.up.isDown) {
+        player.anims.play("misa-back-walk", true);
+    } else if (cursors.down.isDown) {
+        player.anims.play("misa-front-walk", true);
+    } else {
+        player.anims.stop();
+
+        // If we were moving, pick and idle frame to use
+        if (prevVelocity.x < 0) player.setTexture("atlas", "misa-left");
+        else if (prevVelocity.x > 0) player.setTexture("atlas", "misa-right");
+        else if (prevVelocity.y < 0) player.setTexture("atlas", "misa-back");
+        else if (prevVelocity.y > 0) player.setTexture("atlas", "misa-front");
+    }
 }
